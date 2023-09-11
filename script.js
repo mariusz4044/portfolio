b3f9f24fc9d25ec7347c7df720e31ca8700a282b
mariusz42 <mariusz4044@gmail.com>
2023-09-11 15:53:16 +0200

    Add animation show

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-(()=>{
-    const navExperience = document.querySelector('#experience');
-    const navAbout = document.querySelector('#about');
-    const navProjects = document.querySelector('#projects');
-    const navContact = document.querySelector('#contact');
+(() => {
+  const navExperience = document.querySelector("#experience");
+  const navAbout = document.querySelector("#about");
+  const navProjects = document.querySelector("#projects");
+  const navContact = document.querySelector("#contact");
+  const observeElements = document.querySelectorAll(".observeBased");
+  const observeElementsAssets = document.querySelectorAll(".observeAssets");
 
-    navExperience.addEventListener('click',()=>{
-        document.querySelector('#experience-box').scrollIntoView();
+  navExperience.addEventListener("click", () => {
+    document.querySelector("#experience-box").scrollIntoView();
+  });
+
+  navAbout.addEventListener("click", () => {
+    document.querySelector("#about-box").scrollIntoView();
+  });
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (
+        entry.isIntersecting &&
+        entry.target.classList.contains("observeBased")
+      ) {
+        entry.target.classList.add("show");
+      }
+
+      if (
+        entry.isIntersecting &&
+        entry.target.classList.contains("observeAssets")
+      ) {
+        entry.target.classList.add("showAssets");
+      }
     });
+  });
 
-    navAbout.addEventListener('click',()=>{
-        document.querySelector('#about-box').scrollIntoView();
-    });
-
-
+  [...observeElements, ...observeElementsAssets].forEach((element) => {
+    console.log(element);
+    observer.observe(element);
+  });
 })();
